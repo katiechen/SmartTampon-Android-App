39744c2028e3012c4fd45ed10c7601ad19cb3c09
Katie Chen <kcofbek09@gmail.com>
2015-05-06 01:03:02 -0700

    Home page and notifications

---
+++ AndroidStudioProjects/SmartTampon-Katie/app/src/androidTest/java/com/example/madeeha/smarttampon/ApplicationTest.java
@@ -0,0 +1,20 @@
+package com.example.madeeha.smarttampon;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+
+    /** TESTS
+     * are all database functions working properly?
+     * are all Day functions working properly?
+     * are all PeriodChart functions working properly?
+     */
+
+}
